2cf62782ae9b96cc3b346d3884f100f3e148a8ed
mironpuzanob <https://github.com/mironpuzanob/>
2020-06-06 20:44:36 +0300

    Updates

---
+++ precache-manifest.0860abb8fb2986494eb451475b802a97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20b85aa920927142295b71ae4ce2f45",
+    "url": "/kstudio/index.html"
+  },
+  {
+    "revision": "188c01356fa79190aa15",
+    "url": "/kstudio/static/css/2.ad8c1e94.chunk.css"
+  },
+  {
+    "revision": "e4898b0ee835adce5ec8",
+    "url": "/kstudio/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "188c01356fa79190aa15",
+    "url": "/kstudio/static/js/2.7a2422ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kstudio/static/js/2.7a2422ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4898b0ee835adce5ec8",
+    "url": "/kstudio/static/js/main.084f0a03.chunk.js"
+  },
+  {
+    "revision": "7c5e286d2b167a73ad1e",
+    "url": "/kstudio/static/js/runtime-main.4eee3255.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/kstudio/static/media/persik.4e1ec840.png"
+  }
+]);
