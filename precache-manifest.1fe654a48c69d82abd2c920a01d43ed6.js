464a6805db0df40b904d0ad422f7fe391e71ec14
mironpuzanob <https://github.com/mironpuzanob/>
2020-06-07 16:55:59 +0300

    Updates

--- precache-manifest.1fe654a48c69d82abd2c920a01d43ed6.js
+++ precache-manifest.1fe654a48c69d82abd2c920a01d43ed6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d530208752659e360038edd1fdd4b76e",
+    "revision": "369dc26552123721c25d89dd072c90a6",
     "url": "/kstudio/index.html"
   },
   {
-    "revision": "1dd7e3ac370384d010c8",
+    "revision": "7d72f7694b5f767de994",
     "url": "/kstudio/static/css/2.ad8c1e94.chunk.css"
   },
   {
-    "revision": "1dd7e3ac370384d010c8",
-    "url": "/kstudio/static/js/2.2586b5bb.chunk.js"
+    "revision": "7d72f7694b5f767de994",
+    "url": "/kstudio/static/js/2.0f8fd24d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/kstudio/static/js/2.2586b5bb.chunk.js.LICENSE.txt"
+    "url": "/kstudio/static/js/2.0f8fd24d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e97c03ca964799592e7",
-    "url": "/kstudio/static/js/main.a076bf5a.chunk.js"
+    "revision": "139dd07cff36b8c9c512",
+    "url": "/kstudio/static/js/main.9ba8991a.chunk.js"
   },
   {
     "revision": "7c5e286d2b167a73ad1e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kstudio/precache-manifest.5828885a7814b6b987bb8ea6bb25b558.js"
+  "/kstudio/precache-manifest.1fe654a48c69d82abd2c920a01d43ed6.js"
 );
 
 self.addEventListener('message', (event) => {
