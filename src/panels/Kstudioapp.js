fb29f1c48cdccf6556090fa5954c1acab2ef5863
CLUQIE <43346270+CLUQIE@users.noreply.github.com>
2020-06-08 17:14:38 +0300

    Add files via upload

--- src/panels/Kstudioapp.js
+++ src/panels/Kstudioapp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import { Card, Div, CardGrid, Group,Avatar, RichCell, PanelHeaderBack, Text, SimpleCell, Header, Title, Epic, Tabbar, TabbarItem, InfoRow, Headline} from '@vkontakte/vkui';
-
+import Icon16Fire from '@vkontakte/icons/dist/16/fire';
 
 
 import frontendimg from '../img/frontend.jpg';
@@ -18,9 +18,10 @@ const Kstudioapp = ({ id, go}) => (
 		<PanelHeader left={<PanelHeaderBack  style={{color:"#F05C44"}} onClick={go} data-to="project"/>}>
 			K-studio
 		</PanelHeader>
-		<Div style={{outline: '1px solid black', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
-            <img style={{outline: '1px solid black', width: 200, height: 200, display: 'flex', justifyContent: 'center', marginTop: 100}} src={avatars['kstudioapp']}/>
-            <Text style={{fontWeight: 600, fontSize: '14'}}>
+		<Div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
+            <Text style={{display:'flex', marginTop:50, fontWeight: 600, fontSize: 11, color:'#F05C44'}}><Icon16Fire style={{marginTop: 2, marginRight: 5}} fill="#F05C44"/> НОВЫЙ ПРОЕКТ!</Text>
+            <img style={{ width: 200, height: 200, display: 'flex', justifyContent: 'center'}} src={avatars['kstudioapp']}/>
+            <Text style={{fontWeight: 600, fontSize: '14', width:'60%'}}>
             K-studio App - удобное приложение для участников IT-клуба РГУ им. А.Н. Косыгина. <br/><br/>
             Теперь взаимодействие с любимым клубом стало еще проще <br/><br/>
             <span style={{fontWeight: 500, color: '#818C99', fontSize: '14'}}>Разработчик : K-studio team, 2020</span>
