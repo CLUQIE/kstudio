dba432d1da64e4f9b721c6462173a69d81d98f5a
CLUQIE <43346270+CLUQIE@users.noreply.github.com>
2020-06-08 17:27:47 +0300

    Add files via upload

--- src/panels/Kstudioapp.js
+++ src/panels/Kstudioapp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import { Card, Div, CardGrid, Group,Avatar, RichCell, PanelHeaderBack, Text, SimpleCell, Header, Title, Epic, Tabbar, TabbarItem, InfoRow, Headline} from '@vkontakte/vkui';
+import { Card, Div, CardGrid, Button,Avatar, RichCell, PanelHeaderBack, Text, SimpleCell, Header, Title, Epic, Tabbar, FixedLayout, InfoRow, Headline} from '@vkontakte/vkui';
 import Icon16Fire from '@vkontakte/icons/dist/16/fire';
 
 
@@ -21,11 +21,17 @@ const Kstudioapp = ({ id, go}) => (
 		<Div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
             <Text style={{display:'flex', marginTop:50, fontWeight: 600, fontSize: 11, color:'#F05C44'}}><Icon16Fire style={{marginTop: 2, marginRight: 5}} fill="#F05C44"/> НОВЫЙ ПРОЕКТ!</Text>
             <img style={{ width: 200, height: 200, display: 'flex', justifyContent: 'center'}} src={avatars['kstudioapp']}/>
-            <Text style={{fontWeight: 600, fontSize: '14', width:'60%'}}>
+            <Text style={{fontWeight: 600, fontSize: '14', width:'70%'}}>
             K-studio App - удобное приложение для участников IT-клуба РГУ им. А.Н. Косыгина. <br/><br/>
             Теперь взаимодействие с любимым клубом стало еще проще <br/><br/>
             <span style={{fontWeight: 500, color: '#818C99', fontSize: '14'}}>Разработчик : K-studio team, 2020</span>
             </Text>
+            {/* style={{display: 'flex', justifyContent: 'center', width: '100%', outline: '1px solid black'}} */}
+            <FixedLayout vertical="bottom" >
+                <Div>
+                <Button size="xl" vertical="bottom" style={{background:'#F05C44'}}>Отлично</Button>
+                </Div>
+            </FixedLayout>
         </Div>
 		
 	</Panel>
